3443391be49af573172964415377969716867f21
FIRST_NAME LAST_NAME <mitsulandrei@example.com>
2022-10-26 15:12:20 +0300

    1

--- handlers/users/weather_menu.py
+++ handlers/users/weather_menu.py
@@ -5,7 +5,6 @@ from loader import dp
 from data.config import WEATHER_TOKEN
 from keyboards.weather_menu import weather_menu
 from keyboards.inline_kb import inline_kb
-from weather.main_tg_bot import get_weather
 from pprint import pprint
 
 
@@ -14,10 +13,50 @@ from pprint import pprint
 async def show_w_menu(message: types.Message):
     await message.answer('Напиши город или перейди по ссылке', reply_markup=weather_menu)
 
-@dp.message_handler(text='Написать город:')
-async def weather(message: types.Message):
-    await message.answer('В каком городе искать погоду?')
+@dp.message_handler()
+async def get_weather(message: types.Message):
+    get_smile_code = {
+        "Clear": "Ясно \U00002600",
+        "Clouds": "Облачно \U00002601",
+        "Rain": "Дождь \U00002614",
+        "Drizzle": "Дождь \U00002614",
+        "Thunderstorm": "Гроза \U000026A1",
+        "Snow": "Снег \U0001F328",
+        "Mist": "Туман \U0001F32B"
+}
+    try:
+        res = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={message.text}&appid={WEATHER_TOKEN}&units=metric')
+        data = res.json()
+        pprint(data)
 
+        city_name = data["name"]
+        time_now = datetime.datetime.now().strftime('%H.%M')
+        w_description = data["weather"][0]["main"]
+        if w_description in get_smile_code:
+            wd = get_smile_code[w_description]
+        else:
+            wd = 'Посмотри в окно'
+        cur_temp = data["main"]["temp"]
+        humidity = data["main"]["humidity"]
+        pressure = data["main"]["pressure"]
+        wind = data["wind"]["speed"]
+        sunrise = datetime.datetime.fromtimestamp(data["sys"]["sunrise"]).strftime('%H.%M.%S')
+        sunset = datetime.datetime.fromtimestamp(data["sys"]["sunset"]).strftime('%H.%M.%S')
+        length_day = datetime.datetime.fromtimestamp(data["sys"]["sunset"]) - datetime.datetime.fromtimestamp(data["sys"]["sunrise"])
+
+        await message.answer(f'*** Прогноз погоды ***\n'
+              f'Город: {city_name}\n'
+              f'Время: {time_now}\n'
+              f'Температура: {cur_temp}°C. {wd}\n'
+              f'Скорость ветра: {wind} м/с.\n'
+              f'Влажность: {humidity}%.\n'
+              f'Атмосферное давление: {pressure}\n'
+              f'Время рассвета: {sunrise}\n'
+              f'Время заката: {sunset}\n'
+              f'Длительность дня: {length_day}\n'
+              f'*** Хорошего дня ***')
+    except:
+        await message.answer('Ведите правильное название города')
 
 
 @dp.message_handler(text='Ссылка на Gismeteo')
